ff1800f692949ceb12285cb510c89f4020f56fe4
Calvin Langer <calvin.langerr@gmail.com>
2022-07-10 16:20:15 +0200

    add some JS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import './MediaQuerys.css';
 import InputData from './components/InputData';
 import Footer from './components/Footer';
-import SelectData from './components/SelectData';
 import OutputBox from './components/OutoputBox';
 
+// BASAL BMR FORMULA
+// let man_BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+// let woman_BMR = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
 
+const genderOptions = [
+  {
+    value: 'male',
+    label: 'Male'
+  },
+  {
+    value: 'female',
+    label: 'Female'
+  }
+];
 
+const activityOptions = [
+  {
+    value: 'sitting',
+    label: 'Sitting'
+  },
+  {
+    value: 'standing',
+    label: 'Standing'
+  },
+  {
+    value: 'physical',
+    label: 'Physical'
+  }
+];
 
 function App() {
+
+  const [gender, setGender] = useState();
+  const [activity, setActivity] = useState();
+  const [weight, setWeight] = useState();
+  const [height, setHeight] = useState();
+  const [age, setAge] = useState();
+
+  function handleGender(event) {
+    setGender(event.target.value);
+  }
+
+  function handleActivity(event) {
+    setActivity(event.target.value);
+  }
+
+  function handleWeight(event) {
+    setWeight(event.target.value);
+  }
+
+  function handleHeight(event) {
+    setHeight(event.target.value);
+  }
+
+  function handleAge(event) {
+    setAge(event.target.value);
+  }
+
+
   return (
     <div className='wrapper'>
       <div className='subpage-title container'>Calorie calculator</div>
@@ -20,17 +74,39 @@ function App() {
       <div className='container'>
         <form>
           <div className='grid-col-4'>
-            <SelectData id='gender' for='gender' />
-            <InputData labelName="Weight:" id="weight" for="weight" />
-            <InputData labelName="Height:" id="height" for="height" />
-            <InputData labelName="Age:" id="age" for="age" />
+            <div className='grid'>
+
+              <label>Gender:</label>
+              <select value={gender} onChange={handleGender}>
+                {genderOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+
+            </div>
+            <InputData labelName="Weight:" id="weight" for="weight" value={weight} onChange={handleWeight} />
+            <InputData labelName="Height:" id="height" for="height" value={height} onChange={handleHeight} />
+            <InputData labelName="Age:" id="age" for="age" value={age} onChange={handleAge} />
           </div>
+
           <div className='grid-col-4'>
             <OutputBox labelName='Resting metabolism:' />
-            <SelectData id='gender' for='gender' />
+            <div className='grid'>
+              <label>Work performed:</label>
+              <select value={activity} onChange={handleActivity}>
+                {activityOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
             <OutputBox labelName='Daily metabolism:' />
             <InputData labelName="Daily steps(km):" id="steps" for="steps" />
           </div>
+
           <div className='grid-col-4'>
             <OutputBox labelName='Total metabolism:' />
             <div className='grid'>
--- src/components/InputData.js
+++ src/components/InputData.js
@@ -4,7 +4,7 @@ function InputData(props) {
     return (
         <div className="grid">
             <label for={props.for}>{props.labelName}</label>
-            <input type='text' id={props.id} required />
+            <input type='text' id={props.id} value={props.value} required />
         </div >
     );
 }
